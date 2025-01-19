cdcbde535a0cd0e91cccd227c2c27b66db691965
FightWithCode <rahulsharmanoj@gmail.com>
2025-01-19 21:30:20 +0530

    changes

--- finder/serializers.py
+++ finder/serializers.py
@@ -14,10 +14,11 @@ class RecruitmentPostSerializer(serializers.ModelSerializer):
     team_game = serializers.SerializerMethodField()
     team_location = serializers.SerializerMethodField()
     roles = serializers.SerializerMethodField()
+    applied = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = RecruitmentPost
-        fields = ['uuid', 'team_logo', 'team_name', 'team_game', 'team_location', 'created_by', 'title', 'description', 'roles', 'created_at']
+        fields = ['id', 'uuid', 'team_logo', 'team_name', 'team_game', 'team_location', 'applied', 'created_by', 'title', 'description', 'roles', 'created_at']
     def get_team_logo(self, obj):
         return obj.team.logo.url if obj.team.logo else None
     def get_team_name(self, obj):
@@ -28,11 +29,20 @@ class RecruitmentPostSerializer(serializers.ModelSerializer):
         return obj.team.location
     def get_roles(self, obj):
         return [role.name for role in obj.roles.all()]
+    def get_applied(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return RecruitmentApplication.objects.filter(recruitment_post=obj, applicant=user).exists()
+        return False
+
 
 class RecruitmentApplicationSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = RecruitmentApplication
         fields = ['uuid', 'recruitment_post', 'applicant', 'message', 'created_at', 'status']
+        read_only_fields = ['applicant']
+    
 
 class TeamInvitationSerializer(serializers.ModelSerializer):
     class Meta:
