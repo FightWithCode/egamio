a0497aa4488f39c943bddfc30f26e63761770d20
Rahul Kumar Sharma <rahul.sharma@supportlogic.io>
2025-05-05 17:03:59 +0530

    added swagger

--- egamio/urls.py
+++ egamio/urls.py
@@ -16,7 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from egamio.views import home, blogs
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="API documentation for your project",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', home, name='home'),
@@ -26,4 +44,20 @@ urlpatterns = [
     path('finder/', include("finder.urls")),
     path('eg-threads/', include("egthreads.urls")),
     path('games/', include("games.urls")),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+
+
+
+
+
+# urlpatterns = [
+#     # your API urls...
+    
+#     # Swagger UI:
+    
+    
+#     # ReDoc (optional):
+#     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+# ]
